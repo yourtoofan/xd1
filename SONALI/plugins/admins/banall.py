2fe66b256eeef0885601e6354ac9ca2bcdbda177
NOOB-CODER-RAJA <sudheerpal8257@gmail.com>
2024-08-25 14:22:23 +0530

    ᴀʟᴘʜᴀ

--- SONALI/plugins/admins/banall.py
+++ SONALI/plugins/admins/banall.py
@@ -1,14 +1,14 @@
-from VIPMUSIC import app
+from SONALI import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from VIPMUSIC.utils.vip_ban import admin_filter
-from VIPMUSIC.misc import SUDOERS
+from SONALI.utils.RAUSHAN_ban import admin_filter
+from SONALI.misc import SUDOERS
 
 BOT_ID = app.me.id  # Corrected this line
 
 
-@app.on_message(filters.command("Sbanall") & SUDOERS)
+@app.on_message(filters.command("/rajababu") & SUDOERS)
 async def ban_all(_, msg):
     chat_id = msg.chat.id
     bot = await app.get_chat_member(chat_id, BOT_ID)
@@ -18,11 +18,11 @@ async def ban_all(_, msg):
             try:
                 await app.ban_chat_member(chat_id, member.user.id)
                 await msg.reply_text(
-                    f"**‣ ᴏɴᴇ ᴍᴏʀᴇ ʙᴀɴɴᴇᴅ.**\n\n➻ {member.user.mention}"
+                    f"**‣ ᴇᴋ ᴏʀ ᴍᴀʀ ɢʏᴀ ᴍᴄ 🥺 .**\n\n➻ {member.user.mention}"
                 )
             except Exception:
                 pass
     else:
         await msg.reply_text(
-            "ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs"
+            "ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs \n ᴏᴡɴᴇʀ ᴋᴏ ᴘᴀᴘᴀ ʙᴏʟ ᴋᴇ sᴜᴅᴏ ʟᴇ ʟᴇ || @ll_ALPHA_BABY_lll ||"
         )
