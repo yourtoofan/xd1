b87c0e9807014d55911fdc6f868e36699873859a
NOOB-CODER-RAJA <sudheerpal8257@gmail.com>
2024-09-14 15:20:58 +0530

    Update allrepo.py

--- SONALI/plugins/misc/allrepo.py
+++ SONALI/plugins/misc/allrepo.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from PURVIMUSIC import app
+from SONALI import app
 
 # Function to chunk the repository info into smaller parts
 def chunk_string(text, chunk_size):
