a651ef4e05059a3d3a63d0d2dae8cb3a34bb226a
NOOB-CODER-RAJA <sudheerpal8257@gmail.com>
2024-09-18 18:19:20 +0530

    ᴀʟᴘʜᴀ

--- SONALI/utils/stuffs/helper.py
+++ SONALI/utils/stuffs/helper.py
@@ -148,28 +148,3 @@ HELP_Couples = '''ᴄᴏᴜᴘʟᴇs
 ◌ /love : ᴀᴅᴅ ᴛᴡᴏ ɴᴀᴍᴇs ᴀɴᴅ sᴇᴇ ʟᴏᴠᴇ ᴘᴏssɪʙɪʟɪᴛʏ! ʟɪᴋᴇ ʀᴀᴊᴀ + sᴏɴᴀ
 ◌ /couples : ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴀɴᴅ sᴇᴇ ɢʀᴏᴜᴘs ᴄᴏᴜᴘʟᴇs.
 '''
-
-
-    fullpromote = {
-    'can_change_info': True,
-    'can_post_messages': True,
-    'can_edit_messages': True,
-    'can_delete_messages': True,
-    'can_invite_users': True,
-    'can_restrict_members': True,
-    'can_pin_messages': True,
-    'can_promote_members': True,
-    'can_manage_chat': True,
-}
-
-    promoteuser = {
-    'can_change_info': False,
-    'can_post_messages': True,
-    'can_edit_messages': True,
-    'can_delete_messages': False,
-    'can_invite_users': True,
-    'can_restrict_members': False,
-    'can_pin_messages': False,
-    'can_promote_members': False,
-    'can_manage_chat': True,
-}
