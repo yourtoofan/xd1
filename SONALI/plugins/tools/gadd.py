40b7fdc0fa17f92a886ae7db7cf2d84a0f78be38
yourtoofan <toofanpapa424@gmail.com>
2024-10-15 00:09:54 +0530

    Update gadd.py

--- SONALI/plugins/tools/gadd.py
+++ SONALI/plugins/tools/gadd.py
@@ -3,8 +3,8 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 OWNERS = "7520092354"
-from RAUSHAN import app
-from RAUSHAN.utils.database import add_served_chat, get_assistant
+from SONALI import app
+from SONALI.utils.database import add_served_chat, get_assistant
 
 
 @app.on_message(filters.command("gadd") & filters.user(int(OWNERS)))
