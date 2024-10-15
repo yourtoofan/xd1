7215eb051bc765f940e306de34500919a92128a4
yourtoofan <toofanpapa424@gmail.com>
2024-10-15 12:08:31 +0530

    Update userbot.py

--- SONALI/core/userbot.py
+++ SONALI/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("PURVI_SUPPORT")
-                await self.one.join_chat("WORLD_ALPHA")
+                await self.one.join_chat("The_incricible")
+                await self.one.join_chat("link_ka_group")
             except:
                 pass
             assistants.append(1)
