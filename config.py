9961e6edb9ba33e0eb954b6256b6a48f8e4b3b0c
yourtoofan <toofanpapa424@gmail.com>
2024-10-23 12:28:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -96,7 +96,7 @@ PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://envs.sh/nvp.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/9acd828ec45a363add2e9.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/51cb8a22e65caa4382879.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/yfvai0.jpg"
 TELEGRAM_AUDIO_URL = "https://telegra.ph/file/51cb8a22e65caa4382879.jpg"
 TELEGRAM_VIDEO_URL = "https://telegra.ph/file/51cb8a22e65caa4382879.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
